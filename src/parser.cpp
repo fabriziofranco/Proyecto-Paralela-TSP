530ef3eeb29d017c15fa0938a79d34ed414c4d06
fabriziofranco <fabrizio.franco@utec.edu.pe>
2022-11-27 13:49:26 -0500

    Parser

---
+++ src/parser.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <math.h>
+#include "point.cpp"
+
+using namespace std;
+
+vector<string> split(const string &s, char delim) {
+    vector<string> elems;
+    stringstream ss(s);
+    string item;
+    while (getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+    return elems;
+}
+
+
+vector<vector<pair<int,float>>> buildAdjMatrix(int N){
+    string line;
+    vector<string> lines;
+    string path = "data/";
+    ifstream myfile (path + to_string(N) + ".tsp");
+
+    if (myfile.is_open()){
+        for (int i=0; i<8; i++) getline(myfile, line);
+
+        for (int i=0; i<N; i++){
+            getline(myfile, line);
+            lines.push_back(line);
+        }
+
+        myfile.close();
+
+        vector<Point> points;
+        for (int i = 0; i < lines.size(); i++){
+            vector<string> point = split(lines[i], ' ');
+            points.push_back(Point(atoi(point[0].c_str()),
+                                   atoi(point[1].c_str()),
+                                   atoi(point[2].c_str())));
+        }
+
+        vector<vector<pair<int,float>>> adjMatrix;
+        for (int i = 0; i < points.size(); i++){
+            points[i].buildAdj(points);
+            adjMatrix.push_back(points[i].adj);
+        }
+        return adjMatrix;
+    }
+
+    else{
+        cout << "Unable to open file";
+        return vector<vector<pair<int,float>>>();
+    }
+}
+
+
+int main(int argc, char *argv[]){
+    buildAdjMatrix(atoi(argv[1]));
+    return 0;
+}
---
+++ src/point.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <math.h>
+
+using namespace std;
+
+
+struct Point{
+    const float INF = MAXFLOAT;
+    int id,x,y;
+    vector<pair<int,float>> adj;
+
+    Point(int id, int x, int y){
+        this->id = id;
+        this->x = x;
+        this->y = y;
+    }
+
+    void buildAdj(vector<Point> &points){
+        for(int i = 0; i < points.size(); i++){
+            if(points[i].id != this->id){
+                float dist = sqrt(pow(points[i].x - this->x,2) + pow(points[i].y - this->y,2));
+                adj.push_back(make_pair(points[i].id,dist));
+            }
+            else{
+                adj.push_back(make_pair(points[i].id, Point::INF));
+            }
+        }
+    }
+};
