cc22a561262b8bfb5e6fa3926f87336e61e1dcde
fabriziofranco <fabrizio.franco@utec.edu.pe>
2022-11-27 17:01:30 -0500

    Fixing bugs

--- src/branch_bound.cpp
+++ src/branch_bound.cpp
@@ -3,8 +3,6 @@
 
 #include <queue>
 
-const float INFT = MAXFLOAT;
-
 void branch_bound(adj_t mt);
 
 
@@ -15,17 +13,19 @@ int main(int argc, char *argv[]){
     auto adjMt = matrix;
     auto p = reduce_matrix(adjMt);
 
-    vector<float> id{1,2,3,4,5};
-    vector<float> dist1{ INFT, 10,17, 0,1};
-    vector<float> dist2{ 12, INFT,11,2 ,0};
-    vector<float> dist3{ 0, 3,INFT , 0,2};
-    vector<float> dist4{ 15, 3,12, INFT,0};
-    vector<float> dist5{ 11, 0, 0, 12,INFT};
+    vector<int> id{1,2,3,4,5};
+    vector<float> dist{ INFT, 10,17, 0,1,  12, INFT,11,2 ,0,   0, 3,INFT , 0,2,    15, 3,12, INFT,0,   11, 0, 0, 12,INFT};
+
 
     for(int i = 0; i< id.size(); i++){
-        for(int j = 0; j< id.size(); j++){
-            pair<int, float> p{id[i], };
+        for(int j = (id.size()*id[i])-5; j< id.size()*id[i] ; j++){
+            cout<< id[i];
+            cout<< endl;
+            cout<< dist[j];
             
+            
+        }
+    }
         }
     }
 
